10b972ff2074757d6b250a818c2c4fe277bc8728
Michael Heffernan <michael.heffernan.j@gmail.com>
2020-03-28 18:48:13 -0400

    set up redux

---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,17 @@
+const INITITIAL_STATE = {
+  currentUser: null
+};
+
+const userReducer = (state = INITITIAL_STATE, action) => {
+  switch (action.type) {
+    case "SET_CURRENT_USER":
+      return {
+        ...state,
+        currentUser: action.payload
+      };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
