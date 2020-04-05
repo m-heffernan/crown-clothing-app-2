43bd2f64462bc0ccc5481c7a19c1f74e836e02fd
Michael Heffernan <michael.heffernan.j@gmail.com>
2020-04-05 16:32:37 -0400

    refactored with styled components

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -9,7 +9,7 @@ const CustomButton = ({
   ...otherProps
 }) => (
   <button
-    className={` ${inverted ? "inverted" : ""} ${
+    className={`${inverted ? "inverted" : ""} ${
       isGoogleSignIn ? "google-sign-in" : ""
     } custom-button`}
     {...otherProps}
